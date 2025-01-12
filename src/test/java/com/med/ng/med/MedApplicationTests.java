f0a39576fbf3491d32167c4b8c031f2dcc637c38
Nico <nrgaleano00@gmail.com>
2025-01-12 13:16:24 -0300

    Inicializacion del proyecto

---
+++ src/test/java/com/med/ng/med/MedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.med.ng.med;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
